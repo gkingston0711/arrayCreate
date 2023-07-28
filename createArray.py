23d392f46cf8f326e22b64b37032453859d0c547
joey <jkingston90@aol.com>
2023-07-27 19:46:35 -0600

    random 2d array genorator

--- createArray.py
+++ createArray.py
@@ -1,18 +1,32 @@
 import json
+import random
 
 file = open('config.json')
 
 def createArray(D,L,H):
-    print(D)
+
+    myArray =[]
+    #2D array
+    for i in range(L):
+        myArray.append(random.randint(1, 100))
+
+
+    return myArray
+
 
 def main():
     data = json.load(file)
     TwoD = (data['2D'])
     ThreeD = (data['3D'])
-    length = (data['length'])
+    length = int(data['length'])
     height = (data['height'])
 
+    if TwoD:
+        myArray=createArray(TwoD,length,height)
+    else:
+        createArray(ThreeD,length,height)
 
+    print(myArray)
 
     file.close()
 
